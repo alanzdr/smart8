3564d1844bdbb580dc0b6228f974a32176352d48
alanzdr <alandersonzelindrodr@gmail.com>
2020-05-21 17:00:17 -0300

    21/05/2020

---
+++ docs/precache-manifest.fe3dfb51a7fbcf816bfa54188448ce7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa5ea415e492dbcc813435a8933d328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd09cf466019bd0786d4",
+    "url": "/static/css/main.be5a0854.chunk.css"
+  },
+  {
+    "revision": "4023b2c13dee76fc5c01",
+    "url": "/static/js/2.404e5a14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.404e5a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd09cf466019bd0786d4",
+    "url": "/static/js/main.f0dd5b84.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "b54724f54d4dd3f6796e3c4cc422f998",
+    "url": "/static/media/GothamBook.b54724f5.ttf"
+  },
+  {
+    "revision": "544a5860f50695b6068663b2c3295f13",
+    "url": "/static/media/send-icon.544a5860.svg"
+  }
+]);
